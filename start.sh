191d2e4a9ab5c5b436d9b0850359f548c6dffce8
Hdhshsjsjsj <ilashadam039@gmail.com>
2024-07-15 00:38:31 +0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Joelkb/DQ-the-file-donor.git /DQTheFileDonorBot
+  git clone https://github.com/Hdhshsjsjsj/ROCKY.git /ROCKY
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQTheFileDonorBot
+  git clone $UPSTREAM_REPO /ROCKY
 fi
-cd /DQTheFileDonorBot
+cd /ROCKY
 pip3 install -U -r requirements.txt
 echo "Starting DQ-The-File-Donor...."
 python3 bot.py
